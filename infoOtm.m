c104b00c1d0a6a7fbd29d5abf069de0cd1e868b0
gsousa143 <gabrielsousa4242@gmail.com>
2024-03-31 10:35:31 -0300

    simplificando função GA

--- infoOtm.m
+++
@@ -1,9 +0,0 @@
-function infoOtm(it,Fbest,gbest, nome)
-        fprintf("\n\n==============================================================================================================")
-        fprintf('\niter %3.0f; \t fbest  %9.5g; \t gbest ',it,Fbest);
-        fprintf('%9.5g,   ', gbest);
-
-        if nome ~= ""
-            writematrix([it, Fbest, gbest],nome,'WriteMode','append');
-        end
-end
